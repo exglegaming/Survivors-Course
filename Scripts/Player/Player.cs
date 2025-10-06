7ceaf63596267645ec10a5ce81aba75d5ae24bfe
ExgleGaming <EaglesFan91x@outlook.com>
2025-10-05 20:00:22 -0400

    created player and player movement

---
+++ Scripts/Player/Player.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+namespace SurvivorsCourse.Scripts.Player;
+
+public partial class Player : CharacterBody2D {
+    private const float MaxSpeed = 200f;
+    
+    public override void _Ready() {
+        
+    }
+
+    public override void _Process(double delta) {
+        var movementVector = GetMovementVector();
+        var direction = movementVector.Normalized();
+        Velocity = direction * MaxSpeed;
+        MoveAndSlide();
+    }
+
+    private Vector2 GetMovementVector() {
+        var xMovement = Input.GetActionStrength("moveRight") - Input.GetActionStrength("moveLeft");
+        var yMovement = Input.GetActionStrength("moveDown") - Input.GetActionStrength("moveUp");
+        return new Vector2(xMovement, yMovement);
+    }
+}

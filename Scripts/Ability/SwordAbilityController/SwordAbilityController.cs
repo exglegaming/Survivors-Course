49cb5c82c1cdc9202949b5a7289789934254a18a
ExgleGaming <EaglesFan91x@outlook.com>
2025-10-05 23:32:17 -0400

    created sword ability

---
+++ Scripts/Ability/SwordAbilityController/SwordAbilityController.cs
@@ -0,0 +1,22 @@
+using Godot;
+
+namespace SurvivorsCourse.Scripts.Ability.SwordAbilityController;
+
+public partial class SwordAbilityController : Node {
+    [Export] private PackedScene _swordAbility;
+    
+    private Timer _timer;
+    
+    public override void _Ready() {
+        _timer = GetNode<Timer>("Timer");
+        _timer.Timeout += OnTimerTimeout;
+    }
+
+    private void OnTimerTimeout() {
+        if (GetTree().GetFirstNodeInGroup("Player") is not Node2D player) return;
+        
+        var swordInstance = _swordAbility.Instantiate() as Node2D;
+        player.GetParent().AddChild(swordInstance);
+        swordInstance?.GlobalPosition = player.GlobalPosition;
+    }
+}
